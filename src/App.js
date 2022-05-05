f4291641cdfeaa1d05432168d7e6f0552ce65a5f
andrektozzi <andrektozzi@gmail.com>
2022-05-05 11:48:42 -0300

    componentização inicial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Corpo from "./components/Corpo";
+import FundoMobile from "./components/FundoMobile";
+
+
+export default function App () {
+    return (
+        <div>
+            <Navbar />
+            <Corpo />
+            <FundoMobile />
+        </div>
+    )
+}
