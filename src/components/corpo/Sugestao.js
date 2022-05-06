4a5a703d51a73aefbbacce643e919695b624e4e3
andrektozzi <andrektozzi@gmail.com>
2022-05-06 17:17:30 -0300

    componentizaçcompletada

---
+++ src/components/corpo/Sugestao.js
@@ -0,0 +1,14 @@
+export default function Sugestao (props) {
+    return (
+        <div class="sugestao">
+            <div class="usuario">
+                <img src={props.imagem}/>
+                <div class="texto">
+                    <div class="nome">{props.nome}</div>
+                    <div class="nome">{props.razao}</div>
+                </div>
+            </div>
+            <div class="seguir">Seguir</div>
+        </div>
+    )
+}
